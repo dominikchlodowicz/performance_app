d4db2527469b6236e61634913372a4f1e2c2eded
Dominik Chłodowicz <dominikchlodowicz@gmail.com>
2022-07-09 17:05:54 +0200

    start implementing timer

--- performance/main/views.py
+++ performance/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
+from performance.timer_functionality.timer import Timer
 
 from . import main
 @main.route('/')
 def index():
-     return render_template('timer.html')
+     return render_template('timer.html', time=Timer.get_time())
