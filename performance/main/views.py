339c3bba09507b7ab9c2efed3cd3436f19ca307d
Dominik Chłodowicz <dominikchlodowicz@gmail.com>
2022-07-24 14:08:20 +0200

    Separate data handling from client-side to function

--- performance/main/views.py
+++ performance/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for, session, make_response
 from .database import insert_time, get_time
 from . import main
+from ..stopwatch.stopwatch_handling import view_time
 import uuid
 
 @main.route('/')
@@ -15,32 +16,8 @@ def set_user_id():
 
 @main.route('/timer', methods=["GET", "POST"])
 def timer():
-     cookie_user_id = request.cookies.get('user_id')
-     if request.method == 'POST':
-          if request.form['reset'] == 'reset':
-               #getting data from request
-               request_data = request.form['time'].split(':')
-               hours = int(request_data[0])
-               minutes = int(request_data[1])
-               seconds = int(request_data[2])
+     return view_time('timer', 'timer.html')
 
-               #inserting it to database
-               insert_time(cookie_user_id, hours, minutes, seconds)
-               return redirect(url_for('main.timer'))
-
-     elif request.method == 'GET':
-          #try to get cookie
-          if cookie_user_id == None:
-               print('cookie doesn\'t exist')
-               response = make_response(render_template('timer.html'))
-               response.set_cookie('user_id', session['userid'])
-               session['time'] = '00:00:00'
-               return response
-          else:
-               print('cookie exists')
-               time = get_time(cookie_user_id)
-               if time == None:
-                    session['time'] = '00:00:00'
-               else:
-                    session['time'] = time
-               return render_template('timer.html')
+# @main.route('/pomodoro', methods=["GET", "POST"])
+# def pomodoro():
+#      pass
