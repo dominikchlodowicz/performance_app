35ab8dbaaa3b3c6519e5ab0889479c7df9aed890
Dominik Chłodowicz <dominikchlodowicz@gmail.com>
2022-07-29 18:50:41 +0200

    Add cookie handling in frontend.

--- performance/main/views.py
+++ performance/main/views.py
@@ -22,7 +22,9 @@ def timer():
 def pomodoroconfig():          
      if request.method == 'POST':
 
+          # form elements list
           config_values = ('intervals',('workDuration', 'workDurationHrOrMin'), ('breakDuration' ,'breakDurationHrOrMin'))
+          #redirect 
           response = make_response(redirect(url_for('main.pomodoro')))
 
           for form_element_name in config_values:
@@ -30,19 +32,21 @@ def pomodoroconfig():
                if form_element_name == config_values[0]:
                     response.set_cookie(f'{form_element_name}', request.form[form_element_name])
 
+               #validation + data formatting 
                else:
+                    print(f'Now iterating through {form_element_name}')
                     validated_data = validate_time((request.form[form_element_name[0]], request.form[form_element_name[1]]))
                     if validated_data['validation_flag'] == False:
                          session['error'] = validated_data['error']
                          return redirect(url_for('main.pomodoroconfig'))
-                         
+
                     elif validated_data['validation_flag']:
                          session['error'] = None
                          response.set_cookie(f'{form_element_name[0]}', str(data_formatting(validated_data['time'], validated_data['min_or_hr'])))
-                         return response
+                         if form_element_name == config_values[-1]:
+                              return response
 
      elif request.method == 'GET':
-          print('GET method')
           return render_template('config.html')
 
 @main.route('/pomodoro', methods=["GET", "POST"])
--- performance/static/js/pomodoro.js
+++ performance/static/js/pomodoro.js
@@ -1,20 +1,30 @@
-import { 
-        hours,
-        minutes,
-        seconds,
-        displayHours,
-        displayMinutes,
-        displaySeconds,
-        buttonStart,
-        buttonStop,
-        buttonReset,
-        timerValue,
-        Interval
-    } from './another-file.js';
-
 timerValue.value = '0:0:0';
 
-const config_values = ['intervals', 'workDuration', 'workDurationHrOrMin', 'breakDuration' ,'breakDurationHrOrMin'];
+const config_values = ['intervals', 'workDuration', 'breakDuration'];
+
+var cookie = document.cookie;
+
+function getCookie(cookie){
+    const splittedCookiesList = cookie.split("; ");
+    var cookies = {};
+    for(var splittedCookie = 0; splittedCookie < splittedCookiesList.length; splittedCookie++){
+        var cookie_name = '';
+        var cookie_value = '';
+        var flag = false;
+        for(var char = 0; char < splittedCookiesList[splittedCookie].length; char++){
+            if(splittedCookiesList[splittedCookie][char] == "="){
+                flag = true;
+                continue;
+            }
+
+            flag ? cookie_value += splittedCookiesList[splittedCookie][char] : cookie_name += splittedCookiesList[splittedCookie][char];
+        }
+        cookies[cookie_name] = cookie_value;
+    }
+    return cookies;
+}
+
+console.log(getCookie(cookie));
 
 function startPomodoro(numberOfIntervals, durationOfInterval, durationOfIntervalTime,
      durationOfBreak, durationBreakTime){
